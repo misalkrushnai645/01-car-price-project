71d76361e8ab0e49c1d88680ddb048d17baa4f6d
misalkrushnai645 <misalkrushnai645@gmail.com>
2026-06-27 13:52:52 +0530

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import streamlit as st
+
+print("NumPy version:",
+np.__version__)
+print("Pandas version:",
+pd.__version__)
+print("Scikit-learn version:",
+sklearn.__version__)
+print("Streamlit version:",
+st.__version__)
