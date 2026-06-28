84804fd24f003157d928db76afe356517c3512f0
misalkrushnai645 <misalkrushnai645@gmail.com>
2026-06-28 21:51:12 +0530

    Added app.py file

--- app.py
+++ app.py
@@ -11,22 +11,35 @@ car = pd.read_csv("final_data.csv")
 # Sidebar start
 st.sidebar.header("Enter Car Details")
 
-company = st.sidebar.selectbox("Select company",
-    sorted(car["company"].unique()))
-name = st.sidebar.selectbox("Select name",
-    sorted(car["name"].unique()))
-year = st.sidebar.number_input("Enter year", min_value=2000, max_value=2024, 
-                       step=1)
-kms_driven = st.sidebar.number_input("Enter kilometers driven", 
-                             min_value=10000, max_value=400000, 
-                             step = 5000)
-fuel_type = st.sidebar.selectbox("Select fuel type", ["Petrol", "Diesel", "LPG"])
+company = st.sidebar.selectbox(
+    "Select company",
+    sorted(car["company"].unique())
+)
+name = st.sidebar.selectbox(
+    "Select name",
+    sorted(car["name"].unique())
+)
+year = st.sidebar.number_input(
+    "Enter year",
+     min_value=2000,
+     max_value=2024, 
+     step=1
+)
+kms_driven = st.sidebar.number_input(
+    "Enter kilometers driven", 
+     min_value=10000, 
+     max_value=400000, 
+     step = 5000
+)
+fuel_type = st.sidebar.selectbox(
+    "Select fuel type", 
+    ["Petrol", "Diesel", "LPG"])
 
 # Main button
 if st.sidebar.button("Predict Price"):
    model = pkl.load(open("CPP.pkl", "rb"))
    df = pd.DataFrame([[company, name, year, kms_driven, fuel_type]],
-   columns = ['company', 'name', 'year', 'kms_driven', 'fuel_type'])
+   columns = ["company", "name", "year", "kms_driven", "fuel_type"])
    # df = pd.DataFrame(data, columns=columns)
    #st.write(df)
    result = model.predict(df)
