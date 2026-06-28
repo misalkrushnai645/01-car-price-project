325b15e7cb3a3690437e37b70b02c2f4aa2436e0
misalkrushnai645 <misalkrushnai645@gmail.com>
2026-06-28 20:52:23 +0530

    Added app.py file

--- app.py
+++ app.py
@@ -5,18 +5,24 @@ import numpy as np
 import pandas as pd
 
 st.title("Car Price Predictor")
+
 car = pd.read_csv("final_data.csv")
-company = st.selectbox("Select company",
+
+# Sidebar start
+st.sidebar.header("Enter Car Details")
+
+company = st.sidebar.selectbox("Select company",
     sorted(car["company"].unique()))
-name = st.selectbox("Select name",
+name = st.sidebar.selectbox("Select name",
     sorted(car["name"].unique()))
-year = st.number_input("Enter year", min_value=2000, max_value=2024, 
+year = st.sidebar.number_input("Enter year", min_value=2000, max_value=2024, 
                        step=1)
-kms_driven = st.number_input("Enter kilometers driven", 
+kms_driven = st.sidebar.number_input("Enter kilometers driven", 
                              min_value=10000, max_value=400000, 
                              step = 5000)
-fuel_type = st.selectbox("Select fuel type", ["Petrol", "Diesel", "LPG"])
+fuel_type = st.sidebar.selectbox("Select fuel type", ["Petrol", "Diesel", "LPG"])
 
+# Main button
 if st.button("Predict Price"):
     model = pkl.load(open("CPP.pkl", "rb"))
     data = [[company, name, year, kms_driven, fuel_type]]
