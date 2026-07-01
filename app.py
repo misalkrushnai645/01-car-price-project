e7df60782fbba4a56ab2ebc6c056c2b015ef3404
misalkrushnai645 <misalkrushnai645@gmail.com>
2026-07-01 19:08:01 +0530

    Added app.py file

--- app.py
+++ app.py
@@ -48,9 +48,9 @@ if st.sidebar.button("Predict Price"):
     result = pipe.predict(myinput)
 
     if result[0,0] < 0:
-        st.info("Sorry, the predicted price is negative. Please check your input values.")
+        st.write("Sorry, the predicted price is negative. Please check your input values.")
     else:
-        st.info(f"Predicted Price is: ₹ {round(result[0,0])}")
+        st.write(f"Predicted Price is:", str(round(result[0,0])))
           
 
 
